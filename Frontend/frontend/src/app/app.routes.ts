15f11d35557cd7d4675bd183f1dda0e8b3b98144
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2024-05-15 16:06:55 +0200

    Added registration site

--- Frontend/frontend/src/app/app.routes.ts
+++ Frontend/frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'registration', component: RegistrationComponent },
+];
+
