3b43598188611ef80717b132f2a8dda440f6cc1c
Petar M <pmatisic@foi.hr>
2024-05-15 15:06:59 +0200

    second commit

---
+++ jto/src/main/java/hr/stem/jto/JtoApplication.java
@@ -0,0 +1,13 @@
+package hr.stem.jto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtoApplication.class, args);
+	}
+
+}
---
+++ jto/src/test/java/hr/stem/jto/JtoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.stem.jto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
